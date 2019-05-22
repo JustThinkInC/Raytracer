76f30856e08a73a76fbcebafc99c0b7d2fb88439
George K <gkh36@uclive.ac.nz>
2019-05-22 17:45:20 +1200

    Fix cylinder intersection point issues

--- Cylinder.cpp
+++ Cylinder.cpp
@@ -28,6 +28,9 @@ float Cylinder::intersect(glm::vec3 posn, glm::vec3 dir) {
     float t0 = (-b - sqrt(root)) / (2 * a);
     float t1 = (-b + sqrt(root)) / (2 * a);
 
+    if (t0 < 0.01) t0 = -1;
+    if (t1 < 0.01) t1 = -1;
+
     if (t0 > t1) {
         float temp = t0;
         t0 = t1;
@@ -35,18 +38,18 @@ float Cylinder::intersect(glm::vec3 posn, glm::vec3 dir) {
     }
 
     // P = P0 + td;
-    float p = posn.y + t0 * dir.y;
+    float p = posn.y + (t0 * dir.y);
     if ((p >= center.y) && (p <= center.y + height)) {
         return t0;
     }
 
-    p = posn.y + t1 * dir.y;
+    p = posn.y + (t1 * dir.y);
     if ((p >= center.y) && (p <= center.y + height)) {
         return t1;
     }
 
-    return -1;
 
+    return -1;
 }
 
 /**
@@ -54,7 +57,7 @@ float Cylinder::intersect(glm::vec3 posn, glm::vec3 dir) {
 * Assumption: The input point p lies on the cylinder.
 */
 glm::vec3 Cylinder::normal(glm::vec3 p) {
-    glm::vec3 n = glm::vec3(p.x - center.x, 0, p.z - center.z);
+    glm::vec3 n = glm::vec3((p.x - center.x), 0, (p.z - center.z));
     n = glm::normalize(n);
     return n;
 }
