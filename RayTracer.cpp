cb29af702a4ad577a7d909520865478c651872e7
George K <gkh36@uclive.ac.nz>
2019-05-23 12:32:04 +1200

    Working refraction

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -16,37 +16,39 @@
 #include "Cylinder.h"
 #include <GL/glut.h>
 #include "TextureBMP/TextureBMP.h";
+
 using namespace std;
 
-const float WIDTH = 20.0;  
+const float WIDTH = 20.0;
 const float HEIGHT = 20.0;
 const float EDIST = 40.0;
 const int NUMDIV = 500;
 const int MAX_STEPS = 5;
 const float XMIN = -WIDTH * 0.5;
-const float XMAX =  WIDTH * 0.5;
+const float XMAX = WIDTH * 0.5;
 const float YMIN = -HEIGHT * 0.5;
-const float YMAX =  HEIGHT * 0.5;
+const float YMAX = HEIGHT * 0.5;
 
-TextureBMP* wood;
+bool inside = true;
 
-vector<SceneObject*> sceneObjects;  //A global list containing pointers to objects in the scene
+TextureBMP *wood;
+
+vector<SceneObject *> sceneObjects;  //A global list containing pointers to objects in the scene
 
 
 //---The most important function in a ray tracer! ---------------------------------- 
 //   Computes the colour value obtained by tracing a ray and finding its 
 //     closest point of intersection with objects in the scene.
 //----------------------------------------------------------------------------------
-glm::vec3 trace(Ray ray, int step)
-{
-	glm::vec3 backgroundCol(0);
-	glm::vec3 light(10, 40, -3);
-	glm::vec3 ambientCol(0.2);   //Ambient color of light
+glm::vec3 trace(Ray ray, int step) {
+    glm::vec3 backgroundCol(0);
+    glm::vec3 light(10, 40, -3);
+    glm::vec3 ambientCol(0.2);   //Ambient color of light
 
-    ray.closestPt(sceneObjects);		//Compute the closest point of intersetion of objects with the ray
+    ray.closestPt(sceneObjects);        //Compute the closest point of intersetion of objects with the ray
 
 
-    if(ray.xindex == -1) return backgroundCol;      //If there is no intersection return background colour
+    if (ray.xindex == -1) return backgroundCol;      //If there is no intersection return background colour
     glm::vec3 materialCol = sceneObjects[ray.xindex]->getColor(); //else return object's colour
 
     glm::vec3 viewVector = -ray.dir;
@@ -61,93 +63,93 @@ glm::vec3 trace(Ray ray, int step)
     float rDotv = glm::dot(reflVector, viewVector);
 
     float specular;
-    if (rDotv < 0 ) {
+    if (rDotv < 0) {
         specular = 0;
     } else {
         specular = pow(rDotv, 5);
     }
 
+    if (ray.xindex == 5) {
+        float s = (ray.xpt.x);
+        float t = (ray.xpt.y);
+        colorSum = wood->getColorAt(s, t);
+    }
+
+
     Ray shadow(ray.xpt, lightVector);
     shadow.closestPt(sceneObjects);
 
-    if(lDotn <= 0 || shadow.xindex > -1 && shadow.xdist < ray.xdist) {
-        colorSum = ambientCol * materialCol;
+    if (lDotn <= 0 || shadow.xindex > -1 && shadow.xdist < ray.xdist) {
+        colorSum += ambientCol * materialCol;
     } else {
-        colorSum = ambientCol * materialCol + lDotn * materialCol + rDotv * specular;
+        colorSum += ambientCol * materialCol + lDotn * materialCol + rDotv * specular;
     }
 
     if (ray.xindex == 0 && step < MAX_STEPS) {
         glm::vec3 reflectedDir = glm::reflect(ray.dir, normalVector);
         Ray reflectedRay(ray.xpt, reflectedDir);
-        glm::vec3 reflectedCol = trace(reflectedRay, step+1); //Recursion!
+        glm::vec3 reflectedCol = trace(reflectedRay, step + 1); //Recursion!
 
-        colorSum = colorSum + (0.8f*reflectedCol);
+        colorSum = colorSum + (0.8f * reflectedCol);
     }
 
-    if(ray.xindex == 3 && step < MAX_STEPS)
-    {
+
+    if (ray.xindex == 3 && step < MAX_STEPS) {
         // REFRACTION
-//        glm::vec3 n = sceneObjects[ray.xindex]->normal(ray.xpt);
-        glm::vec3 g = glm::refract(ray.dir, normalVector, 1/1.01f);
+        float eta;
+        if (inside) {
+            eta = 1/1.003;
+            normalVector = -normalVector;
+        } else {
+            eta = 1.003;
+        }
+        glm::vec3 g = glm::refract(ray.dir, normalVector, eta);
         Ray refrRay(ray.xpt, g);
-        refrRay.closestPt(sceneObjects);
-        if (refrRay.xindex == -1) {
-            return backgroundCol;
-        }
+        glm::vec3 refracCol = trace(refrRay, step + 1);
 
-        glm::vec3 m = sceneObjects[refrRay.xindex]->normal(refrRay.xpt);
-        glm::vec3 h = glm::refract(g, -m, 1.01f);
-        Ray refrRay2(refrRay.xpt, h);
-        refrRay2.closestPt(sceneObjects);
-        if (refrRay2.xindex == -1) {
-            return backgroundCol;
-        }
-        glm::vec3 refrCol = trace(refrRay2, step+1);
-
-        colorSum = colorSum + refrCol;
+        colorSum += refracCol;
     }
-
 
 
     return colorSum;
 }
 
+
 //---The main display module -----------------------------------------------------------
 // In a ray tracing application, it just displays the ray traced image by drawing
 // each cell as a quad.
 //---------------------------------------------------------------------------------------
-void display()
-{
-	float xp, yp;  //grid point
-	float cellX = (XMAX-XMIN)/NUMDIV;  //cell width
-	float cellY = (YMAX-YMIN)/NUMDIV;  //cell height
+void display() {
+    float xp, yp;  //grid point
+    float cellX = (XMAX - XMIN) / NUMDIV;  //cell width
+    float cellY = (YMAX - YMIN) / NUMDIV;  //cell height
 
-	glm::vec3 eye(0., 0., 0.);  //The eye position (source of primary rays) is the origin
+    glm::vec3 eye(0., 0., 0.);  //The eye position (source of primary rays) is the origin
 
-	glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-	glBegin(GL_QUADS);  //Each cell is a quad.
+    glBegin(GL_QUADS);  //Each cell is a quad.
 
-	for(int i = 0; i < NUMDIV; i++)  	//For each grid point xp, yp
-	{
-		xp = XMIN + i*cellX;
-		for(int j = 0; j < NUMDIV; j++)
-		{
-			yp = YMIN + j*cellY;
+    for (int i = 0; i < NUMDIV; i++)    //For each grid point xp, yp
+    {
+        xp = XMIN + i * cellX;
+        for (int j = 0; j < NUMDIV; j++) {
+            yp = YMIN + j * cellY;
 
-		    glm::vec3 dir(xp+0.5*cellX, yp+0.5*cellY, -EDIST);	//direction of the primary ray
+            glm::vec3 dir(xp + 0.5 * cellX, yp + 0.5 * cellY, -EDIST);    //direction of the primary ray
 
-		    Ray ray = Ray(eye, dir);		//Create a ray originating from the camera in the direction 'dir'
-			ray.normalize();				//Normalize the direction of the ray to a unit vector
-		    glm::vec3 col = trace (ray, 1); //Trace the primary ray and get the colour value
+            Ray ray = Ray(eye, dir);        //Create a ray originating from the camera in the direction 'dir'
+            ray.normalize();                //Normalize the direction of the ray to a unit vector
 
-			glColor3f(col.r, col.g, col.b);
-			glVertex2f(xp, yp);				//Draw each cell with its color value
-			glVertex2f(xp+cellX, yp);
-			glVertex2f(xp+cellX, yp+cellY);
-			glVertex2f(xp, yp+cellY);
+            glm::vec3 col = trace(ray, 1); //Trace the primary ray and get the colour value
+
+            glColor3f(col.r, col.g, col.b);
+            glVertex2f(xp, yp);                //Draw each cell with its color value
+            glVertex2f(xp + cellX, yp);
+            glVertex2f(xp + cellX, yp + cellY);
+            glVertex2f(xp, yp + cellY);
         }
     }
 
@@ -162,19 +164,18 @@ void display()
 //   It also initializes the OpenGL orthographc projection matrix for drawing the
 //     the ray traced image.
 //----------------------------------------------------------------------------------
-void initialize()
-{
+void initialize() {
     glMatrixMode(GL_PROJECTION);
     gluOrtho2D(XMIN, XMAX, YMIN, YMAX);
     Sphere *sphere2 = new Sphere(glm::vec3(14.0, 10.0, -80.0), 5.0, glm::vec3(0, 1, 0));
     Sphere *sphere3 = new Sphere(glm::vec3(5.0, 5.0, -70.0), 3, glm::vec3(1, 0, 0));
     Sphere *sphere4 = new Sphere(glm::vec3(5.0, -15.0, -70.0), 4, glm::vec3(1, 0.65, 0));
 
-    Plane *plane = new Plane (glm::vec3(-20., -20, -40),    //Point A
-                              glm::vec3(20., -20, -40),     //Point B
-                              glm::vec3(20., -20, -200),    //Point C
-                              glm::vec3(-20., -20, -200),   //Point D
-                              glm::vec3(0.5, 0.5, 0));      //Colour
+    Plane *plane = new Plane(glm::vec3(-20., -20, -40),    //Point A
+                             glm::vec3(20., -20, -40),     //Point B
+                             glm::vec3(20., -20, -200),    //Point C
+                             glm::vec3(-20., -20, -200),   //Point D
+                             glm::vec3(0.5, 0.5, 0));      //Colour
     glClearColor(0, 0, 0, 1);
 
     //-- Create a pointer to a sphere object
@@ -184,11 +185,11 @@ void initialize()
 
     Cylinder *cylinder = new Cylinder(glm::vec3(-5, -15, -70), 2, 3, glm::vec3(1, 1, 1));
 
-    wood = new TextureBMP("/assets/brick_1.bmp");
+    wood = new TextureBMP("../assets/brick_1.bmp");
 
     sphere1->refractionIndex = 1.1003;
 
-	//--Add the above to the list of scene objects.
+    //--Add the above to the list of scene objects.
     sceneObjects.push_back(sphere1);
     sceneObjects.push_back(sphere2);
     sceneObjects.push_back(sphere3);
@@ -199,10 +200,9 @@ void initialize()
 }
 
 
-
 int main(int argc, char *argv[]) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB );
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(600, 600);
     glutInitWindowPosition(20, 20);
     glutCreateWindow("Raytracer");
